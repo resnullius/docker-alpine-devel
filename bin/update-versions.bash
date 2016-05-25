549fadb37d0a34bcca85c360d096364b4b11e464
Jose-Luis Rivas <me@ghostbar.co>
2016-05-25 15:00:49 -0500

    bin/update-versions: fix error on cp -R

--- bin/update-versions.bash
+++ bin/update-versions.bash
@@ -38,7 +38,7 @@ create_tag() {
 run_updater() {
   for ver in $VERSIONS_CHILDS; do
     echo "Copying scripts from $VERSIONS_BASE to $ver"
-    cp -R "$VERSIONS_BASE/" "$ver"
+    cp -R "$VERSIONS_BASE"/* "$ver"
     [[ "$ver" = "versions/armv7l" ]] && create_tag "$ver"
     echo "Done on $ver"
   done
